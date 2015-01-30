c6c60f21092b365136ad2627d6bba7b7ffe3b505
ryan <ryanmce@gmail.com>
2015-01-30 07:45:06 -0800

    scm-prompt.sh: recognize more states

--- scm-prompt.sh
+++ scm-prompt.sh
@@ -14,12 +14,13 @@
 #  export PS1='$(_dotfiles_scm_info)$USER@%m:%~%% '
 
 # To use from bash:
-#  NOTE! the single quotes are important; if you use double quotes
-#  then the prompt won't change when you chdir or checkout different
-#  branches!
 #
 #  . /mnt/vol/engshare/admin/scripts/scm-prompt
-#  export PS1='$(_dotfiles_scm_info)\u@\h:\W\$ '
+#  export PS1="\$(_dotfiles_scm_info)\u@\h:\W\$ "
+#
+# NOTE! You *EITHER* need to single-quote the whole thing *OR* back-slash
+# the $(...) (as above), but not both. Which one you use depends on if
+# you need the rest of your PS1 to interpolate variables.
 
 _dotfiles_scm_info()
 {
@@ -41,42 +42,82 @@ _dotfiles_scm_info()
 
   local br
   if test -n "$hg" ; then
-    br=$(hexdump -n 5 -e '1/1 "%02x"' $hg/.hg/dirstate)
-    local file
-    for file in "$hg/.hg/bookmarks.current"; do
-      test -f "$file" && { read br < "$file" ; break; }
-    done
+    local extra
+    if [ -f "$hg/.hg/bisect.state" ]; then
+      extra="|BISECT"
+    elif [ -f "$hg/.hg/histedit-state" ]; then
+      extra="|HISTEDIT"
+    elif [ -f "$hg/.hg/graftstate" ]; then
+      extra="|GRAFT"
+    elif [ -f "$hg/.hg/unshelverebasestate" ]; then
+      extra="|UNSHELVE"
+    elif [ -f "$hg/.hg/rebasestate" ]; then
+      extra="|REBASE"
+    elif [ -d "$hg/.hg/merge" ]; then
+      extra="|MERGE"
+    fi
+    local dirstate=$(test -f $hg/.hg/dirstate && \
+      hexdump -vn 20 -e '1/1 "%02x"' $hg/.hg/dirstate || \
+      echo "empty")
+    local current="$hg/.hg/bookmarks.current"
+    if  [[ -f "$current" ]]; then
+      br=$(cat "$current")
+      # check to see if active bookmark needs update
+      local marks="$hg/.hg/bookmarks"
+      if [[ -z "$extra" ]] && [[ -f "$marks" ]]; then
+        local markstate=$(grep " $br$" "$marks" | cut -f 1 -d ' ')
+        if [[ $markstate != $dirstate ]]; then
+          extra="|UPDATE_NEEDED"
+        fi
+      fi
+    else
+      br=$(expr substr "$dirstate" 1 7)
+    fi
+    br="$br$extra"
   elif test -n "$git" ; then
     if test -f "$git/.git/HEAD" ; then
       read br < "$git/.git/HEAD"
       case $br in
         ref:\ refs/heads/*) br=${br#ref: refs/heads/} ;;
-        *)
-          r="DETACHED"
-          if [ -f "$git/.git/rebase-merge/interactive" ]; then
-            r="REBASE-i"
-            b="$(cat "$git/.git/rebase-merge/head-name")"
-          elif [ -d "$git/.git/rebase-merge" ]; then
-            r="REBASE-m"
-            b="$(cat "$git/.git/rebase-merge/head-name")"
-          else
-            if [ -d "$git/.git/rebase-apply" ]; then
-              if [ -f "$git/.git/rebase-apply/rebasing" ]; then
-                r="REBASE"
-              elif [ -f "$git/.git/rebase-apply/applying" ]; then
-                r="AM"
-              else
-                r="AM/REBASE"
-              fi
-            elif [ -f "$git/.git/MERGE_HEAD" ]; then
-              r="MERGING"
-            elif [ -f "$git/.git/BISECT_LOG" ]; then
-              r="BISECTING"
-            fi
-          fi
-        br="${br:0:7}|$r" ;;
+        # Lop off all of an SHA1 except the leading 7 hex digits.
+        # Use this cumbersome notation (it's portabile) rather
+        # than ${br:0:7}, which doesn't work with older zsh.
+        *) br="${br%????????????????????????????????}" ;;
       esac
+      if [ -f "$git/.git/rebase-merge/interactive" ]; then
+        b="$(cat "$git/.git/rebase-merge/head-name")"
+        b=${b#refs/heads/}
+        br="$br|REBASE-i|$b"
+      elif [ -d "$git/.git/rebase-merge" ]; then
+        b="$(cat "$git/.git/rebase-merge/head-name")"
+        b=${b#refs/heads/}
+        br="br|REBASE-m|$b"
+      else
+        if [ -d "$git/.git/rebase-apply" ]; then
+          if [ -f "$git/.git/rebase-apply/rebasing" ]; then
+            br="$br|REBASE"
+          elif [ -f "$git/.git/rebase-apply/applying" ]; then
+            br="$br|AM"
+          else
+            br="$br|AM/REBASE"
+          fi
+        elif [ -f "$git/.git/MERGE_HEAD" ]; then
+          br="$br|MERGE"
+        elif [ -f "$git/.git/BISECT_LOG" ]; then
+          br="$br|BISECT"
+        fi
+      fi
     fi
   fi
-  test -n "$br" && printf %s "($br)" || :
+  # Be compatable with __git_ps1. In particular:
+  # - provide a space for the user so that they don't have to have
+  #   random extra spaces in their prompt when not in a repo
+  # - provide parens so it's differentiated from other crap in their prompt
+  if [ -n "$br" ]; then
+    if [ -n "$WANT_OLD_SCM_PROMPT" ]; then
+      printf %s "$br"
+    else
+      printf ' (%s)' "$br"
+    fi
+  fi
 }
